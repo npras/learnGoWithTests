d26f7ed225d6fbc54284af5206e2940a9c865369
Prasanna.Natarajan <prasanna.natarajan@lookout.com>
2024-06-16 08:07:32 +0530

    sums

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -1,9 +1,33 @@
 package main
 
+//import "fmt"
+
 func Sum(arr []int) int {
   result := 0
   for _, v := range arr {
     result += v
+  }
+  return result
+}
+
+
+func SumAll(numsToSum ...[]int) []int {
+  var result []int
+  for _, v := range numsToSum {
+    result = append(result, Sum(v))
+  }
+  return result
+}
+
+
+func SumAllTails(numsToSum ...[]int) []int {
+  var result []int
+  for _, v := range numsToSum {
+    if len(v) == 0 {
+      result = append(result, 0)
+    } else {
+      result = append(result, Sum(v[1:]))
+    }
   }
   return result
 }
--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,7 +1,10 @@
 package main
 
 
-import "testing"
+import (
+  "testing"
+  "slices"
+)
 
 
 func TestSum(t *testing.T) {
@@ -12,5 +15,36 @@ func TestSum(t *testing.T) {
     if got != want {
       t.Errorf("got %d want %d given, %v", got, want, numbers)
     }
+  })
+}
+
+
+func TestSumAll(t *testing.T) {
+  got := SumAll([]int{1, 2, 3}, []int{4, 5, 6})
+  want := []int{6, 15}
+  if !slices.Equal(got, want) {
+    t.Errorf("got: %v, want: %v", got, want)
+  }
+}
+
+
+func TestSumAllTails(t *testing.T) {
+  checkSums := func(t testing.TB, got, want []int) {
+    t.Helper()
+    if !slices.Equal(got, want) {
+      t.Errorf("got: %v, want: %v", got, want)
+    }
+  }
+
+  t.Run("non-empty arrs", func(t *testing.T) {
+    got := SumAllTails([]int{1, 2, 3}, []int{4, 5, 6})
+    want := []int{5, 11}
+    checkSums(t, got, want)
+  })
+
+  t.Run("empty arrs", func(t *testing.T) {
+    got := SumAllTails([]int{}, []int{})
+    want := []int{0, 0}
+    checkSums(t, got, want)
   })
 }
