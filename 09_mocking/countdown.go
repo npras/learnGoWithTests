c60e7ec33be1364642b76f951f16422b921f28e5
Prasanna.Natarajan <prasanna.natarajan@lookout.com>
2024-07-01 07:37:16 +0530

    mocking wip

--- 09_mocking/countdown.go
+++ 09_mocking/countdown.go
@@ -20,20 +20,22 @@ type Sleeper interface {
 }
 
 
-type DefaultSleeper struct {}
-
-
-func (s *DefaultSleeper) Sleep() {
-  time.Sleep(1 * time.Second)
-}
-
-
 func Countdown(out io.Writer, sleeper Sleeper) {
   for i := countdownStart; i > 0; i-- {
     fmt.Fprintln(out, i)
     sleeper.Sleep()
   }
   fmt.Fprint(out, finalWord)
+}
+
+
+//
+
+type DefaultSleeper struct {}
+
+
+func (s *DefaultSleeper) Sleep() {
+  time.Sleep(1 * time.Second)
 }
 
 
