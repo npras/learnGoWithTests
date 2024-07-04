380d20506819265a02531fc29716a13bab72c893
pras <prasanna@npras.in>
2024-07-04 12:29:49 +0530

    mocking done

--- 09_mocking/countdown.go
+++ 09_mocking/countdown.go
@@ -32,7 +32,17 @@ func (s *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (cs *ConfigurableSleeper) Sleep() {
+	cs.sleep(cs.duration)
+}
+
 func main() {
-	sleeper := &DefaultSleeper{}
+	//sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{5 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
--- 09_mocking/countdown_test.go
+++ 09_mocking/countdown_test.go
@@ -2,32 +2,72 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"reflect"
 	"testing"
+	"time"
 )
 
-type SpySleeper struct {
-	Calls int
+const (
+	sleepStr = "zzzzz\n"
+)
+
+type SpecialSpy struct {
+	Outputs []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (ss *SpecialSpy) Sleep() {
+	ss.Outputs = append(ss.Outputs, sleepStr)
+}
+
+func (ss *SpecialSpy) Write(written []byte) (n int, err error) {
+	ss.Outputs = append(ss.Outputs, string(written[:]))
+	return
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
 }
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
-	Countdown(buffer, spySleeper)
-
-	got := buffer.String()
-	want := `3
+	t.Run("prints 3, 2, 1, Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpecialSpy{}
+		Countdown(buffer, spySleeper)
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
+		if got != want {
+			t.Errorf("got: %q, want: %q", got, want)
+		}
+	})
 
-	if got != want {
-		t.Errorf("got: %q, want: %q", got, want)
-	}
-	if spySleeper.Calls != 3 {
-		t.Errorf("spySleeper calls: got: %d, want: 3", spySleeper.Calls)
-	}
+	t.Run("prints 3, 2, 1, Go!", func(t *testing.T) {
+		spy := SpecialSpy{}
+		spySleeper := &spy
+		spyWriter := &spy
+		Countdown(spyWriter, spySleeper)
+		got := spy.Outputs
+		fmt.Println(got)
+		want := []string{"3\n", sleepStr, "2\n", sleepStr, "1\n", sleepStr, "Go!"}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got: %q, want: %q", got, want)
+		}
+	})
 }
