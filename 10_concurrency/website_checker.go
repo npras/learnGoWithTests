938229bba4f3fada5f1edc82a765e776f724c9a1
pras <prasanna@npras.in>
2024-07-05 07:50:37 +0530

    concurrency done

--- 10_concurrency/website_checker.go
+++ 10_concurrency/website_checker.go
@@ -2,10 +2,30 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := map[string]bool{}
 	for _, url := range urls {
-		results[url] = wc[url]
-		return results
+		results[url] = wc(url)
 	}
+	return results
+}
+
+func CheckWebsitesWithChannel(wc WebsiteChecker, urls []string) map[string]bool {
+	results := map[string]bool{}
+	resultChannel := make(chan result)
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+	return results
 }
--- 10_concurrency/website_checker_test.go
+++ 10_concurrency/website_checker_test.go
@@ -1,8 +1,9 @@
 package concurrency
 
 import (
+	"reflect"
 	"testing"
-	//"reflect"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
@@ -13,9 +14,45 @@ func mockWebsiteChecker(url string) bool {
 }
 
 func TestCheckWebsites(t *testing.T) {
-	websites = []string{
+	websites := []string{
 		"https://google.com",
 		"https://bing.com",
 		"http://whatthe.com",
+	}
+	got := CheckWebsites(mockWebsiteChecker, websites)
+	want := map[string]bool{
+		"https://google.com": true,
+		"https://bing.com":   true,
+		"http://whatthe.com": false,
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got: %v, want: %v", got, want)
+	}
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "some url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
+
+func BenchmarkCheckWebsitesWithChannel(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "some url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsitesWithChannel(slowStubWebsiteChecker, urls)
 	}
 }
